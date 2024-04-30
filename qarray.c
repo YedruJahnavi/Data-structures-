585e5dce21a5fdd87353b66d4afd6b86b782e173
YedruJahnavi <166897449+YedruJahnavi@users.noreply.github.com>
2024-04-30 10:47:29 +0530

    Update queues using arrays

--- qarray.c
+++ qarray.c
@@ -10,31 +10,30 @@ void delete();
 void display();
 int main()
 {
-	int choice
-	printf("insert element\n");
-	printf("delete element\n");
-	printf("display element);
-	printf("exit\n");
-	printf("enter your choice\n");
-	scanf("%d",&item);
-	switch(choice)
+	int choice,item;
+	while(1)
 	{
-		case 1: printf("enter the element:\n");
-			scanf("%d",item);
-			insert(item);
-			break;
-		case 2: printf("enter the element:\n');
-			scanf("%d",item);
-			delete();
-			break;
-		case 3: printf("enter the element:\n);
-			scanf("%d",item);
-			display();
-			break;
-		case 4: exit(0);
-		default: ("invalid choice:\n");
+		printf("\nMENU\n");
+		printf("insert element\n");
+		printf("delete element\n");
+		printf("display element\n");
+		printf("exit\n");
+		printf("enter your choice\n");
+		scanf("%d",&choice);
+		switch(choice)
+			{
+				case 1: printf("enter the element:\n");
+					scanf("%d",&item);
+					insert(item);
+					break;
+				case 2: delete();
+					break;
+				case 3: display();
+					break;
+				case 4: exit(0);
+				default: ("invalid choice:\n");
+			}
 	}
-}
 void insert(int item)
 {
 	if(rear=MAX-1)
@@ -49,15 +48,29 @@ void insert(int item)
 void delete()
 {
 	if(front==-1)
-		printf("queue is empty\n);
+		printf("queue is empty\n");
 	else
 	{
-		printf("delete element:\n);
+		printf("delete element:%d\n",queue[front]);
 		if(front==rear)
 			front=rear=-1;
 		else
-		{
-			printf(
+			front++;
+	}
+}
+void dispay()
+{
+	int i;
+	if(front==-1)
+		printf("queue is empty\n");
+	else
+	{
+		printf("queue:\n");
+		for(i=front;i<=rear;i++);
+			printf("%d",queue[i]);
+	}
+}
+}
 
 
 
